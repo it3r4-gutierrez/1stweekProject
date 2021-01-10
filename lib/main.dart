f0d69d39bf1a46e090c0639a218ab43f9895dd8c
alyssa <alyssa.gutierrez.731@gmail.com>
2021-01-10 23:57:57 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Model.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,14 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+var counter=0;
+
+ List qList =[
+    //Questions( Image.asset("assets/images/brain.png"), false),
+   
+
+  ];
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -92,13 +101,18 @@ class HomeScreen extends StatelessWidget {
   }   
   }
 
-  
-
   void _navigateToNextScreen(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => DialogExample()));
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewScreen()));
   }
 
-  
+  void _navigateToNextScreen2(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewScreen2()));
+  }
+
+    void _navigateToNextScreen3(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewScreen3()));
+  }
+
 
 class NewScreen extends StatelessWidget {
   @override
@@ -106,67 +120,91 @@ class NewScreen extends StatelessWidget {
     return MaterialApp(
       home: new Scaffold(
         backgroundColor: Colors.cyan,
-    
-      body: Container(
-        child: Row(
-          children: [
-            Container(
-                  alignment: Alignment(-1,-0.90),
-                  child: Image.asset('assets/images/home.png',
-                  height: 100,
-                  width: 50,
-                  ),
-                ),
-          Container(
-                alignment: Alignment(0.4,-0.80),
-                child: Text('SCIENCE IS FUN',
-                style: new TextStyle(
-                  fontSize: 30.0,
-                  ),
-                )
-              ,
-            ),
-        
-         Row(children: [
-           
-         ],)
-          
-          ],
-        ),
-      )
-
+         appBar: AppBar(
+      
       ),
 
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              child: Text('SCIENCE is FUN',
+              style: new TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Roboto',
+                color: new Color(0xDD000000))
+                )
+            ),
+            new Container(
+              child: Text('click to learn'),
+            ),
+            new Container(
+               decoration: new BoxDecoration(
+               color: Colors.blue ),
+               child: new Text('categories',
+               textAlign: TextAlign.center,
+               style: new TextStyle(
+                  fontSize: 30.0
+                ),)
+            ),
+            new Container(
+          
+          padding: EdgeInsets.all(16.0),
+          child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          shrinkWrap: true,
+          
+          
+          children: <Widget>[
+            //Image.asset("assets/images/human_body.png",),
+            FlatButton(
+            child: Image.asset("assets/images/human_body.png",),
+            onPressed: () {
+            _navigateToNextScreen2 (context);
+          },
+              ),
 
+
+            Image.asset("assets/images/food.png"),
+            Image.asset("assets/images/animals.png"),
+            Image.asset("assets/images/plants.png"),
+            Image.asset("assets/images/planets.png"),
+            Image.asset("assets/images/games.png"),
+
+            ],
+           )
+          )
+         ],
+        ),
+       )
+      ),
     );
   }
 }
 
-class HumanBody extends StatelessWidget {
+class NewScreen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: new Scaffold(
         backgroundColor: Colors.cyan,
+         appBar: AppBar(
       
-      body: Container(
-       child: Column(
+      ),
+
+      body: Center(
+        child: Column(
           children: [
-              new Align(
-                  alignment: Alignment(-0.70,1),
-                  child: Image.asset('assets/images/home.png',
-                  height: 100,
-                  width: 50,)
-              ),
-            new Align(
-              alignment: Alignment.topCenter,
+            Container(
               child: Text('HUMAN BODY ORGANS',
               style: new TextStyle(
-                fontSize: 30.0,
+                fontSize: 20.0,
+                fontFamily: 'Roboto',
+                color: new Color(0xDD000000))
                 )
-              )
             ),
-             new Container(
+            new Container(
           
           padding: EdgeInsets.all(16.0),
           child: GridView.count(
@@ -176,83 +214,130 @@ class HumanBody extends StatelessWidget {
           
           
           children: <Widget>[
-           IconButton(
-                    icon: Image.asset('assets/images/brain.png'),
-                    iconSize: 50,
-                    onPressed: () {
-                     
-                    },
-                    ),
+            Image.asset("assets/images/brain.png",),
             Image.asset("assets/images/lungs.png"),
             Image.asset("assets/images/kidney.jpg"),
             Image.asset("assets/images/intestine.png"),
             Image.asset("assets/images/stomach.jpg"),
-            Image.asset("assets/images/heart.png"),
+            Image.asset("assets/images/heart.png"), 
+             ],
+              ),
+              
+            ),
+            new Container(
+            width: 200,
+            height: 50,
+            
+            child : RaisedButton(
+            
+            child: Text("Next "),
+            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+            color: Colors.blue,
+            onPressed: () {
+              },
+              ),
+            ),
 
-          ],
-        )
+            new Container(
+            width: 200,
+            height: 50,
+            
+            child : RaisedButton(
+            
+            child: Text("Play",),
+            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+            color: Colors.blue,
+            onPressed: () { _navigateToNextScreen3 (context);
+              },
+              ),
             )
 
           ],
         ),
       )
     ),
-  );
-}
-}
 
-class DialogExample extends StatefulWidget {
 
-  @override
-  _DialogExampleState createState() => new _DialogExampleState();
-}
-
-class _DialogExampleState extends State<DialogExample> {
-  String _text = "initial";
-  TextEditingController _c;
-  @override
-  initState(){
-    _c = new TextEditingController();
-    super.initState();
+    );
   }
+}
+
+class NewScreen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(_text),
-            new RaisedButton(onPressed: () {
-              showDialog(child: new Dialog(
-                child: new Column(
-                  children: <Widget>[
-                    new TextField(
-                        decoration: new InputDecoration(hintText: "Update Info"),
-                        controller: _c,
-
-                    ),
-                    new FlatButton(
-                      child: new Text("Save"),
-                      onPressed: (){
-                        setState((){
-                        this._text = _c.text;
-                      });
-                      Navigator.pop(context);
-                      },
-                    )
-                  ],
-                ),
-
-              ), context: context);
-            },child: new Text("Enter your name"),)
-          ],
-        )
+    return MaterialApp(
+      home: new Scaffold(
+        backgroundColor: Colors.cyan,
+         appBar: AppBar(
+      
       ),
+
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              child: Text('HUMAN BODY ORGANS \n GAME',
+              style: new TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Bold',
+                color: new Color(0xDD000000))
+                )
+            ),
+            new Container(
+          
+          padding: EdgeInsets.all(16.0),
+          child: GridView.count(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10.0,
+          shrinkWrap: true,
+          
+          
+          children: <Widget>[
+            new Container(
+               child: TextFormField( style: TextStyle(color: Colors.white),
+             ),
+                decoration: BoxDecoration(
+                  
+                  image: DecorationImage(fit: BoxFit.cover,
+                  image: AssetImage("assets/images/brain.png",),
+                  ),  
+                ),
+              )
+            // Image.asset("assets/images/brain.png",),
+            // Image.asset("assets/images/lungs.png"),
+            // Image.asset("assets/images/kidney.jpg"),
+            // Image.asset("assets/images/intestine.png"),
+            // Image.asset("assets/images/stomach.jpg"),
+            // Image.asset("assets/images/heart.png"), 
+             ],
+              ),
+              
+            ),
+         
+
+          ],
+        ),
+      )
+    ),
+
+
     );
   }
 }
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
